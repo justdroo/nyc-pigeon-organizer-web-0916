5303232bfdd172ab23e4ce77ef0e9104d7153f50
Drew Nickerson <dnick13@gmail.com>
2016-09-26 23:59:48 -0400

    First Attempt - incomplete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  name_array_all = []
+
+#adding all names to an array
+  data.each do |key, value|
+    value.each do |key2, value2|
+      name_array_all << value2
+    end
+  end
+
+#finding unique names and creating first layer of new_hash
+  name_final = name_array_all.flatten.uniq
+  name_final.each do |name|
+    new_hash[name] = {color: [], gender: [], lives: []}
+  end
+
+#adding colors to new_hash
+
+
+#returns the whole hash
+new_hash
 end
+
+puts nyc_pigeon_organizer(pigeon_data)

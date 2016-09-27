ba2a0a9492987de6ec651d18fa0904ec2819512e
Drew Nickerson <dnick13@gmail.com>
2016-09-27 17:07:15 -0400

    Complete - After lecture

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -46,3 +46,55 @@ end
 #returns the whole hash
 new_hash
 end
+
+
+#alternative -- notes from group meeting
+  #build a scaffold hash that will be shoved into names
+  #create hash with names
+  #assign each name its scaffold hash
+    #fill scaffold hash
+
+#alternative -- notes from lecture
+  # A. Problem Sovling
+    # rephrase the problem
+    # think about how we solve this problem without code
+      # use an analogy OR how this would work in real life
+
+  # B. Translating this into code
+    # REJECT information - just to get to core
+    # Coerce - think about hte data structure that we want
+
+  # General Techniques
+    # making the problem less abstract
+      # puts, binding
+      # write a comment of the data return value
+    # get a feedback loop
+    # really understanding the data structure 
+
+def nyc_pigeon_organizer_two(data)
+# MAP will return a changed value, EACH will return original
+
+  # coerce each name as the key in the hash
+  #access all names
+  #names - ['']
+    # unique list of names
+  names = data.values[0].values.flatten.uniq
+
+    # use name as key in hash - fill in with scaffold hash
+  initial_structure = names.each_with_object ({}) do |name, hash|
+    hash[name] = {color: [], gender: [], lives:[]}
+  end
+
+  names.each_with_object(initial_structure) do |pigeon, my_initial_structure|
+    attributes = data.keys
+    attributes.each do |attribute|
+      data[attribute].each do |value, names|
+        if names.include?(pigeon)
+          my_initial_structure[pigeon][attribute] << value.to_s
+        end
+      end
+    end
+  end
+
+
+end
